ffb5634626f31d4352a8e81e7fc937f40e9b1d62
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-01-17 13:39:24 +0900

    hmm

--- colla.h
+++ colla.h
@@ -26,16 +26,21 @@ TP(uint T, T L) union streamt { T ret; c buf[L]; };
 
 TP(uint T, T L = sizeof(T), TN G = streamt<T, L>) IL RG T TFG(RG s f) {
     FILE* fp = fopen(f, r);
-    if (fp == NULL) { return nullptr; };
+    if (fp == NULL) { return nullptr; }; //file must be L B.
     
     RG G fv;
-    fgets(fv.buf, L, fp);
+    fgets(fv.buf, L, fp); //file must be L B.
     
     fclose(fp);
     return fv.ret;
 }
 
-TP(uint T) IL RG T collatz(RG T x) { return x-1?collatz(x&1?++(x + x<<1):x>>1):0; }
+TP(uint T) IL RG T pp(RG T x) {
+    x++;
+    return x;
+}
+
+TP(uint T) IL RG T collatz(RG T x) { return x-1?collatz(x&1?pp(x + x<<1):x>>1):0; }
 
 TP(uint T) IL RG i collatzm(RG s f) { return (i) collatz<T>(TFG<T>(f)); }
 
